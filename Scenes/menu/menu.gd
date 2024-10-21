ba5bd4260c85a2e799253fbc49a1f496ff711676
omcpeake <72383307+omcpeake@users.noreply.github.com>
2024-10-21 17:32:13 +0100

    added menu

---
+++ Scenes/menu/menu.gd
@@ -0,0 +1,22 @@
+class_name Menu
+extends Control
+@export var default_button : Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	default_button.grab_focus()
+
+
+
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
