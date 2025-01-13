5123057cddfa12252d87a63823223253d53e10d2
omcpeake <72383307+omcpeake@users.noreply.github.com>
2025-01-13 17:41:09 +0000

    added respawn button
    added a button to respawn the player from the pause menu

--- Scenes/UI/pause_menu.gd
+++ Scenes/UI/pause_menu.gd
@@ -11,9 +11,24 @@ func _ready():
 func _process(delta):
 	pass
 
-
-
-
-func _on_resume_button_pressed():
+func unpause():
 	get_tree().paused = false
 	queue_free()
+	
+func _on_resume_button_pressed():
+		unpause()
+
+
+func _on_respawn_button_pressed():
+	var player = get_tree().get_first_node_in_group("Player")
+	if player is Player:
+		unpause()
+		player.health_component.kill()
+
+
+func _on_restart_level_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_to_menu_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/UI/menu.tscn")
