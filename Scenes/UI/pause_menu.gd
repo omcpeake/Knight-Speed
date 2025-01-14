af24fdd91778e0047c13aee062ebd4840172f5fe
omcpeake <72383307+omcpeake@users.noreply.github.com>
2025-01-14 10:54:22 +0000

    favicon and small updates

--- Scenes/UI/pause_menu.gd
+++ Scenes/UI/pause_menu.gd
@@ -2,16 +2,8 @@ extends CanvasLayer
 class_name PauseMenu
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func unpause():
+	## unpauses the game and deletes the menu
 	get_tree().paused = false
 	queue_free()
 	
@@ -24,10 +16,6 @@ func _on_respawn_button_pressed():
 	if player is Player:
 		unpause()
 		player.health_component.kill()
-
-
-func _on_restart_level_button_pressed():
-	pass # Replace with function body.
 
 
 func _on_quit_to_menu_button_pressed():
